f56d5b1f9262a8460639f956791f1a81bccee057
Loki Astari <Loki.Astari@gmail.com>
2016-07-26 21:44:08 +0000

    Improved rule for Class/Stuct checking.
        Class's are identified so that Constructor/Destructor
        are not included as normal functions when checking for
        an initial lower case letter

--- rules/M001.tcl
+++ rules/M001.tcl
@@ -84,8 +84,16 @@ proc isBuiltInType {s} {
     return [expr [lsearch $builtInTypes $s] != -1]
 }
 
+proc lpop listVar {
+        upvar 1 $listVar l
+        set r [lindex $l end]
+        set l [lreplace $l [set l end] end] ; # Make sure [lreplace] operates on unshared object
+        return $r
+}
+
 set state "start"
 set pp_line -1
+lappend classState std
 
 foreach f [getSourceFileNames] {
     foreach t [getTokens $f 1 0 -1 -1 {}] {
@@ -112,6 +120,38 @@ foreach f [getSourceFileNames] {
             } else {
                 #puts "$tokenName -> $identifier: => Typedef Continue"
             }
+        } elseif {$state == "start" && ($tokenName == "struct" || $tokenName == "class")} {
+            set state "Class1"
+            # puts "$tokenName -> $identifier: => Class Start"
+        } elseif {$state == "Class1" && ($tokenName == "space" || $tokenName == "newline")} {
+            set state "Class2"
+            # puts "$tokenName -> $identifier: => Class Space"
+        } elseif {$state == "Class2" && $tokenName == "identifier"} {
+            set state "Class3"
+            set className $identifier
+            # puts "$tokenName -> $identifier: => Class Identifier"
+        } elseif {$state == "Class3" && ($tokenName == "space" || $tokenName == "newline")} { 
+            set state "Class4"
+            # puts "$tokenName -> $identifier: => Class Extra Space"
+        } elseif {$state == "Class4" && ($tokenName == "leftbrace" || $tokenName == "colon")} {
+            set state "start"
+            lappend classState $className
+            # puts "$tokenName -> $identifier: => Class Pushing $className"
+        } elseif {$state == "Class4"} {
+            set state "start"
+            # puts "$tokenName -> $identifier: => Class Back to Start"
+        } elseif {$state == "start" && $tokenName == "rightbrace"} {
+            set state "CheckClassClose"
+            # puts "$tokenName -> $identifier: => Class CheckDone Start"
+        } elseif {$state == "CheckClassClose" && ($tokenName == "space" || $tokenName == "newline")} {
+            #ignore
+            # puts "$tokenName -> $identifier: => Class CheckDone Space"
+        } elseif {$state == "CheckClassClose" && $tokenName == "semicolon"} {
+            lpop classState
+            # puts "$tokenName -> $identifier: => Class CheckDone Pop"
+        } elseif {$state == "CheckClassClose"} {
+            set state "start"
+            # puts "$tokenName -> $identifier: => Class CheckDone Start"
         } elseif {$state == "start" && $tokenName == "template"} {
             set state "Template"
             #puts "$tokenName -> $identifier: => Template Start"
@@ -178,7 +218,11 @@ foreach f [getSourceFileNames] {
             }
             set identifier [lindex $t 0]
             set identifierFirst [string index $identifier 0]
-            if {$ignore == 0 && [expr ! [string is lower $identifierFirst]]} {
+            # puts "Checking $identifier"
+            if {[lsearch -exact $classState $identifier] > 0} {
+                # puts "Constructor Destructor"
+            } elseif {$ignore == 0 && [expr ! [string is lower $identifierFirst]]} {
+                # puts "Failed $identifier"
                 report $f $lineNumber "Objects >$identifier< (variables/functions) should have an initial lower case letter"
             }
         } else {
